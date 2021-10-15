5746960ceb7a62a6ebe8919224139e660c5cc8ac
malltaf <malltaf@malltaf.com>
2021-10-16 01:05:19 +0300

    Added installation of k.2

--- zsh-oh-installer.sh
+++ zsh-oh-installer.sh
@@ -47,7 +47,7 @@ function downloadmod(){
     echo; echo "Download k";
     git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM}/plugins/k
     # Set a less bright color for dir for k
-	sed -i 's/K_COLOR_DI="0;34"/K_COLOR_DI="0;94"/g' ${ZSH_CUSTOM}/plugins/k/k.sh
+    sed -i 's/K_COLOR_DI="0;34"/K_COLOR_DI="0;94"/g' ${ZSH_CUSTOM}/plugins/k/k.sh
 
     { [[ $ZTHEME ]] && thmcheck; } || { [[ $ZEASY ]] && ZSH_DEFAULT_THEME="robbyrussell"; } || { thmenter; }
     echo "Theme you entered is $ZSH_DEFAULT_THEME"; echo;
